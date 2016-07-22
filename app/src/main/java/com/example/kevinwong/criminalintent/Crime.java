1253c530e782029584e9f61a47eacd8afd1c4563
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-21 18:08:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kevinwong/criminalintent/Crime.java
@@ -0,0 +1,17 @@
+package com.example.kevinwong.criminalintent;
+import java.util.UUID;
+
+/**
+ * Created by kevinwong on 2016-07-21.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+
+}
