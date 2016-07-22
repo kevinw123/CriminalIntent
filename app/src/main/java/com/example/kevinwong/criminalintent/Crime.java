be19eb9daca57addab1abf12d4eaf38ffc1f493d
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-21 22:47:23 -0700

    Adding widgets and modifying UI

--- app/src/main/java/com/example/kevinwong/criminalintent/Crime.java
+++ app/src/main/java/com/example/kevinwong/criminalintent/Crime.java
@@ -1,6 +1,6 @@
 package com.example.kevinwong.criminalintent;
 import java.util.UUID;
-
+import java.util.Date;
 /**
  * Created by kevinwong on 2016-07-21.
  */
@@ -8,10 +8,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
-
+    private Date mDate;
+    private boolean mSolved;
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
+
 
     public UUID getId(){
         return mId;
@@ -23,5 +26,23 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date GetDate(){
+        return mDate;
+    }
+
+    public void setDate(Date date)
+    {
+        mDate = date;
+    }
+
+    public boolean isSolved(){
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved)
+    {
+        mSolved = solved;
     }
 }
