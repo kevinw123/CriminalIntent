8d77039a6c272d5e2b8c963bf564c84cffb60891
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-28 21:28:54 -0700

    Implementing current crime for clicked view

--- app/src/main/java/com/example/kevinwong/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/kevinwong/criminalintent/CrimePagerActivity.java
@@ -48,6 +48,13 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        for(int i = 0; i < mCrimes.size(); i++){
+            if(mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 
 }
